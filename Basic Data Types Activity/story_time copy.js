f268433e984da6e7082265028cd1530f90f00963
spectere13 <isabellareddy@icloud.com>
2022-02-04 23:50:38 -0800

    new file

---
+++ Basic Data Types Activity/story_time copy.js
@@ -0,0 +1,44 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+
+var ourName = "Shun";
+var cityName = "Najenndel";
+var characterName = "Four";
+var capitalName = "Erwan";
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+
+var years = 17;
+var revolutions = 5;
+var leader = 3;
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+
+var socialStatus = ['king', 'guard', 'general', 'leader', 'citizen'];
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+
+var rumor = false
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log("- Hello- " + ourName + "?");
+console.log("= Yes, that's me.");
+console.log("- Wonderful. Welcome to " + cityName + ", home of the illustrious " + characterName + ".");
+console.log("= ... of course.");
+console.log("- Ah. Well, since you already have your papers, sent straight to us from " + capitalName + ", I'm sure there's no need\nfor formalities. I'll show you to the palace.");
+console.log("= Papers- right." + "\n");
+
+console.log("= ... sooo, who're the Four again?");
+console.log("- You haven't heard? They're the longest lasting dynasty of this country. They settled the throne between them\n" + years + " years ago. Spread peace throughout the lands, and ended the " + revolutions + " major revolutions.");
+console.log("= But who are they?");
+console.log("- ... well, there's 4 of them.");
+console.log("= I guessed.");
+console.log("- And- people say that the last revolution never really went away. They recruited it's leader for\ntheir " + leader + "rd " + socialStatus[0] + ".");
+console.log("= Huh.");
+console.log("- Then, the people also say that each king gains a life from a past dynasty's blessing.");
+console.log("= I'm going to go out on a limb and say that that rumor's " + rumor + ".");
+console.log("- ...");
+console.log("= Wait, seriously? Wha-");
+console.log("- Oh, look! We're almost there." + "\n");
+
+console.log("- Welcome, to the outer courtyard of Ranjiling Palace.");
